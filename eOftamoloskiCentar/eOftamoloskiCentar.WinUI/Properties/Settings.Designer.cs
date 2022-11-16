e81ca1a3abbd4b9a54aeee6ba3ea21ba113024e7
naida-merzic <94006697+naida-merzic@users.noreply.github.com>
2022-11-16 19:13:12 +0100

    update

--- eOftamoloskiCentar/eOftamoloskiCentar.WinUI/Properties/Settings.Designer.cs
+++ eOftamoloskiCentar/eOftamoloskiCentar.WinUI/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace eOftamoloskiCentar.WinUI.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://localhost:7031/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:5192/")]
         public string ApiURL {
             get {
                 return ((string)(this["ApiURL"]));
